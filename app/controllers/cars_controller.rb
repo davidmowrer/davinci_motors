4990d70c90cbb14d4b1499e2a8557984d5af44da
David Mowrer <david.mowrer@comcast.net>
2014-11-07 18:13:01 -0700

    Changes for homework assignment

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,6 +1,8 @@
 class CarsController < ApplicationController
+  before_action :find_car, only: [:edit, :update]
 
   def index
+    @cars = Car.all
   end
 
   def new
@@ -9,10 +11,23 @@ class CarsController < ApplicationController
 
   def create
     @car = Car.new(car_params)
+    @car.save
     redirect_to cars_path,
       notice: "#{@car.year} #{@car.make} #{@car.model} created"
   end
 
+  def show
+    @car = Car.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+      @car.update(car_params)
+        redirect_to cars_path,
+      notice: 'Car was successfully updated.'
+  end
 
   private
 
@@ -20,4 +35,7 @@ class CarsController < ApplicationController
     params.required(:car).permit(:year, :make, :model, :price)
   end
 
+  def find_car
+    @car = Car.find(params[:id])
+  end
 end
--- spec/features/creating_cars_spec.rb
+++ spec/features/creating_cars_spec.rb
@@ -13,10 +13,69 @@ feature 'Creating Cars' do
 
     click_button 'Create Car'
 
-     expect(page).to have_content('1967 Ford Mustang created')
-    # expect(page).to have_content('Mustang')
-    # expect(page).to have_content('Mustang')
-    # expect(page).to have_content('Mustang')
-    # expect(page).to have_content('Mustang')
+    expect(page).to have_content('1967 Ford Mustang created')
+
+    within('table') do
+      expect(page).to have_content('Ford')
+      expect(page).to have_content('Mustang')
+      expect(page).to have_content('1967')
+      expect(page).to have_content('2300')
+    end
+  end
+
+  scenario "can create a second car" do
+    Car.create(
+    make: 'Ford',
+    model: 'Mustang',
+    year: '1967',
+    price:'2300'
+    )
+    visit '/'
+
+    click_link 'New Car'
+
+    fill_in 'Make', with: 'Pontiac'
+    fill_in 'Model', with: 'GTO'
+    fill_in 'Year', with: '1964'
+    fill_in 'Price', with: '27500'
+
+    click_button 'Create Car'
+
+    expect(page).to have_content('1964 Pontiac GTO created')
+
+    within('table') do
+
+      expect(page).to have_content('Ford')
+      expect(page).to have_content('Mustang')
+      expect(page).to have_content('1967')
+      expect(page).to have_content('2300')
+
+      expect(page).to have_content('Pontiac')
+      expect(page).to have_content('GTO')
+      expect(page).to have_content('1964')
+      expect(page).to have_content('27500')
+    end
+  end
+end
+
+feature 'Editing cars' do
+  scenario 'Edits car information year' do
+    car = FactoryGirl.create(:car)
+    visit '/'
+
+    click_link 'Edit'
+
+    fill_in 'Make',  with: 'Ford'
+    fill_in 'Model', with: 'Mustang'
+    fill_in 'Year',  with: '1967'
+    fill_in 'Price', with: '2300'
+
+    fill_in 'Year', with: '1968'
+    click_button 'Update'
+
+    expect(page).to have_content('Ford')
+    expect(page).to have_content('Mustang')
+    expect(page).to have_content('1968')
+    expect(page).to have_content('2300')
   end
 end
